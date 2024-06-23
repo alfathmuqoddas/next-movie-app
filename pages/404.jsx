5bb07b435455f524a3d02ebc3c6199596c24243f
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2024-06-23 19:14:33 +0700

    update

--- pages/404.jsx
+++ pages/404.jsx
@@ -12,7 +12,9 @@ const index = () => {
         <div className="mx-4">
           <div className="flex items-center justify-center h-[calc(100vh-11rem)]">
             <div>
-              <h1 className="text-2xl">The Page You're Looking Is Not Exist</h1>
+              <h1 className="text-2xl">
+                The Page You&apos;re Looking Is Not Exist
+              </h1>
               <div className="flex items-center justify-center">
                 <button className="btn btn-outline rounded-full mr-2 mt-4">
                   <Link href="/">Back To Home</Link>
