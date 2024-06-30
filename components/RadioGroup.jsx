84e522d05f8b7c37dab1c9e844f9e97460593ee6
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2024-07-01 00:37:08 +0700

    update

--- components/RadioGroup.jsx
+++ components/RadioGroup.jsx
@@ -1,19 +1,33 @@
-const RadioGroup = ({ name, options }) => {
+import { useState } from "react";
+
+const RadioGroup = ({ name, options, onChange }) => {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setSelectedValue(value);
+    onChange(value);
+  };
+
   return (
-    <div role="group" aria-labelledby={radioGroupName}>
+    <div role="group" aria-labelledby={name}>
       <div className="flex align-center gap-4">
-        <label>
-          <input
-            type="radio"
-            name="trending-radio"
-            value="today"
-            id="trending-radio"
-            className="peer sr-only"
-          />
-          <div className="rounded-[99px] text-white ring-1 ring-white peer-checked:bg-white transition-all peer-checked:text-black peer-checked:ring-white py-1 px-6">
-            Today
-          </div>
-        </label>
+        {options.map((option) => (
+          <label key={option.value}>
+            <input
+              type="radio"
+              name={name}
+              value={option.value}
+              id={name}
+              className="peer sr-only"
+              checked={selectedValue === option.value}
+              onChange={handleChange}
+            />
+            <div className="rounded-[99px] text-white ring-1 ring-white peer-checked:bg-white transition-all peer-checked:text-black peer-checked:ring-white py-1 px-6">
+              {option.label}
+            </div>
+          </label>
+        ))}
       </div>
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import Head from "next/head";
 import Link from "next/link";
@@ -8,6 +9,7 @@ import {
   getTrendingData,
 } from "../lib/getData";
 import { TemplateFront2 } from "../components/TemplateFront";
+import RadioGroup from "../components/RadioGroup";
 
 export const getStaticProps = async () => {
   const popularDatas = await getPopularData("movie");
@@ -15,6 +17,7 @@ export const getStaticProps = async () => {
   const nowPlayingDatas = await getNowPlayingData();
   const topRatedDatas = await getTopRatedData();
   const trendingDatas = await getTrendingData();
+  const trendingDatasWeek = await getTrendingData("week", 1);
 
   return {
     props: {
@@ -23,6 +26,7 @@ export const getStaticProps = async () => {
       nowPlayingDatas,
       topRatedDatas,
       trendingDatas,
+      trendingDatasWeek,
     },
     revalidate: 3600,
   };
@@ -34,7 +38,19 @@ export default function Index({
   topRatedDatas,
   popularTvDatas,
   trendingDatas,
+  trendingDatasWeek,
 }) {
+  const [isTrendingToday, setTrendingToday] = useState(true);
+
+  const trendingOptions = [
+    { value: true, label: "Today" },
+    { value: false, label: "This Week" },
+  ];
+
+  const handleChangeTrendingTime = (value) => {
+    setTrendingToday(value);
+  };
+  console.log(isTrendingToday);
   return (
     <div>
       <Head>
@@ -44,15 +60,28 @@ export default function Index({
         <div className="py-8">
           <div className="flex justify-between items-center px-8">
             <h3 className="text-2xl font-extrabold">Trending</h3>
+            <RadioGroup
+              options={trendingOptions}
+              name="trending-option"
+              onChange={handleChangeTrendingTime}
+            />
             <Link href="/trending">
               <h4 className="hover:underline">See More →</h4>
             </Link>
           </div>
-          <TemplateFront2
-            content={trendingDatas}
-            seeAll="trending"
-            contentLink=""
-          />
+          {isTrendingToday ? (
+            <TemplateFront2
+              content={trendingDatas}
+              seeAll="trending"
+              contentLink=""
+            />
+          ) : (
+            <TemplateFront2
+              content={trendingDatasWeek}
+              seeAll="trending"
+              contentLink=""
+            />
+          )}
         </div>
         <div className="py-8">
           <TemplateFront2
